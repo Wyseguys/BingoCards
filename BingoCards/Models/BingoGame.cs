b5d4224c4b3f9f4a3f7c80f1c5eec7e4f1d5a3d2
Wyseguys <wyseguys@gmail.com>
2022-10-23 18:52:02 -0700

    Clear unneeded launch options for debugging

--- BingoCards/Models/BingoGame.cs
+++ BingoCards/Models/BingoGame.cs
@@ -2,14 +2,13 @@
 {
     public class BingoGame
     {
-        public int Id { get; set; } 
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string? Name { get; set; } = null;
         public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime Updated { get; set; }=DateTime.Now;
+        public DateTime Updated { get; set; } = DateTime.Now;
         public bool IsWinner { get { return CheckNormalWin(); } }
         public bool IsBlackout { get { return CheckBlackout(); } }
         public bool HasFreeSpace { get; set; } = true;
-        public bool IsReady { get; set; } = false;
         public List<BingoSquare>? Squares { get; set; }
         public int Rows { get; set; }
         public int Columns { get; set; }
@@ -21,18 +20,28 @@
         }
         public BingoGame(List<string> wordlist, string name = "dude", string freespacelabel = "FREE SPACE")
         {
-            Words = wordlist ?? throw new ArgumentNullException(nameof(wordlist));
-            if (!IsSaneGame()) throw new Exception("Word list should be 9, 16 or 25 entries");
-            double sides = Math.Sqrt(Words.Count);
+            try
+            {
+                Words = wordlist ?? throw new ArgumentNullException(nameof(wordlist));
+                if (!IsSaneGame()) throw new Exception("Word list should be 9, 16 or 25 entries");
+                double sides = Math.Sqrt(Words.Count);
 
-            Squares = SetupSquares(sides, freespacelabel);
-            Rows = Convert.ToInt32(sides);
-            Columns = Convert.ToInt32(sides);
-            Name = name;
+                Squares = SetupSquares(sides, freespacelabel);
+                Rows = Convert.ToInt32(sides);
+                Columns = Convert.ToInt32(sides);
+                Name = name;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Cannot create new bingo card: " + ex.Message);
+            }
+
         }
 
-
-
+        /// <summary>
+        /// Check if every square is marked on the bingo card
+        /// </summary>
+        /// <returns>bool (true or false)</returns>
         public bool CheckBlackout()
         {
             if(Squares == null) return false;
@@ -43,6 +52,10 @@
             return false;
         }
 
+        /// <summary>
+        /// Check if there is a full row, column or diagonal win
+        /// </summary>
+        /// <returns>bool (true or false)</returns>
         public bool CheckNormalWin()
         {
             if (Squares == null) return false;
@@ -50,6 +63,10 @@
             return CheckHorizontalWin() || CheckVerticalWin() || CheckTopDownDiagonalWin() || CheckBottomUpDiagonalWin();
         }
 
+        /// <summary>
+        /// See if the diagonal, top left towards bottom right win condition is met
+        /// </summary>
+        /// <returns>bool (true or false)</returns>
         private bool CheckTopDownDiagonalWin()
         {
             if (Squares == null) return false;
@@ -64,6 +81,10 @@
             return false;
         }
 
+        /// <summary>
+        /// See if the diagonal, bottom left up towards the top right win condition is met
+        /// </summary>
+        /// <returns>bool (true or false)</returns>
         public bool CheckBottomUpDiagonalWin()
         {
             if (Squares == null) return false;
@@ -80,6 +101,10 @@
             return true;
         }
 
+        /// <summary>
+        /// Check if all the columnns in any of the rows have met the win condition
+        /// </summary>
+        /// <returns>bool (true or false)</returns>
         private bool CheckHorizontalWin()
         {
             if (Squares == null) return false;
@@ -94,6 +119,10 @@
             return false;
         }
 
+        /// <summary>
+        /// Check if all the rows in any of the columns have met the win condition
+        /// </summary>
+        /// <returns>bool (true or false)</returns>
         private bool CheckVerticalWin()
         {
             if (Squares == null) return false;
@@ -108,6 +137,10 @@
             return false;
         }
 
+        /// <summary>
+        /// Confirm this game is either 9, 16 or 25 squares.  Reject anything else
+        /// </summary>
+        /// <returns>bool (true or false)</returns>
         private bool IsSaneGame()
         {
             //I didn't write this, ye old auto formatter suggested it. 
