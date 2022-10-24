4fee6d4d92f01e7cf0010cec03dd62e63f105f05
Wyseguys <wyseguys@gmail.com>
2022-10-23 17:03:47 -0700

    Manually tidy up some source code

--- BingoCards/Utilities/ShuffleList.cs
+++ BingoCards/Utilities/ShuffleList.cs
@@ -3,14 +3,12 @@
     //
     // https://stackoverflow.com/questions/273313/Randomize-a-listt
     //
-
-
     public static class ThreadSafeRandom
     {
         [ThreadStatic] private static Random? Local;
         public static Random ThisThreadsRandom
         {
-            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+            get { return Local ??= new Random(unchecked(Environment.TickCount * 31 + Environment.CurrentManagedThreadId)); }
         }
     }
     static class ShuffleList
@@ -22,9 +20,7 @@
             {
                 n--;
                 int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
+                (list[n], list[k]) = (list[k], list[n]);
             }
         }
     }
